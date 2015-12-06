417b7f8f1116f37c02671f37ce43c5d1ea131cba
appidi <appidiabhinav@users.noreply.github.com>
2015-12-06 16:06:24 +0530

    Update DCM.c

--- DCM.c
+++ DCM.c
@@ -1,28 +1,3 @@
-@@ -0,0 +1,262 @@
-/********************************************************
- Source code .c file Library for DCM matrix attitude 
- - Based on William Premerlani paper and Jose Julio code for DCM matrix
- 	
-    Copyright (C) 2014  Juan Lopez Medina
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-* You can contact me by mail : julome21@gmail.com
-*********************************************************/
-
-
 // Mode 0 for out angle only gyro 
 // Mode 1 for out angles gyro + accel
 // Mode 2 for out angles only accelerometer
