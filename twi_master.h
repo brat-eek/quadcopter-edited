fc4466bd7c6eb7aa85bdb9199604ce0295890148
appidi <appidiabhinav@users.noreply.github.com>
2015-12-06 15:59:13 +0530

    Update twi_master.h

--- twi_master.h
+++ twi_master.h
@@ -1,34 +1,9 @@
-@@ -0,0 +1,111 @@
-/*********************************************************************
 					Library for TWI AVR
 		Based on AVR315 - TWI Master Implementation Atmel Corporation
 		
 		Improved:
 				- Time out for not hold in loop
 				- Function TWI_Get_Data_From_Transceiver modified
-			Copyright (C) 2014  Juan Lopez Medina
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-	You can contact me in julome21@gmail.com
-
-    Library for TWI AVR  Copyright (C) 2014  Juan Lopez Medina
-	
-    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
-    This is free software, and you are welcome to redistribute it
-    under certain conditions; type `show c' for details.
- ****************************************************************************/ 
 
 /*****************************************************************************
 	Use TWI_Master_Initialise(); in your setup code for initalize the TWI
